cd5cfbe1a7db70f9e6d1c863d883696f368261e0
knqzx <nov-kir.n@yandex.ru>
2022-10-30 16:43:45 +0300

    add main page

--- main.py
+++ main.py
@@ -6,10 +6,36 @@ import sys
 app = Flask(__name__)
 
 
+@app.route('/authorization', methods=['POST', 'GET'])
+def authorization():
+    if request.method == 'GET':
+        return render_template('index.html')
+
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html', url='')
+        list_data = [[{'image_url': 'https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvYmFmeWJlaWQ1MmppaXU2Z3ZybHh0YWtmN3h5MnJ2Ynh3eGkzNHNzdjd2c2h3MzVsM2hvaGRkdDY3bGUvaW1hZ2UucG5n',
+                      'name': 'Doctor Strange',
+                      'description': 'Ticket to Doctor Strange premier'},
+                     {
+                         'image_url': 'https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvYmFmeWJlaWQ1MmppaXU2Z3ZybHh0YWtmN3h5MnJ2Ynh3eGkzNHNzdjd2c2h3MzVsM2hvaGRkdDY3bGUvaW1hZ2UucG5n',
+                         'name': 'Doctor Strange',
+                         'description': 'Ticket to Doctor Strange premier'},
+                     {
+                         'image_url': 'https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvYmFmeWJlaWQ1MmppaXU2Z3ZybHh0YWtmN3h5MnJ2Ynh3eGkzNHNzdjd2c2h3MzVsM2hvaGRkdDY3bGUvaW1hZ2UucG5n',
+                         'name': 'Doctor Strange',
+                         'description': 'Ticket to Doctor Strange premier'}],
+                     [{
+                         'image_url': 'https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvYmFmeWJlaWQ1MmppaXU2Z3ZybHh0YWtmN3h5MnJ2Ynh3eGkzNHNzdjd2c2h3MzVsM2hvaGRkdDY3bGUvaW1hZ2UucG5n',
+                         'name': 'Doctor Strange',
+                         'description': 'Ticket to Doctor Strange premier'},
+                     {
+                         'image_url': 'https://assets.raribleuserdata.com/prod/v1/image/t_image_big/aHR0cHM6Ly9pcGZzLmlvL2lwZnMvYmFmeWJlaWQ1MmppaXU2Z3ZybHh0YWtmN3h5MnJ2Ynh3eGkzNHNzdjd2c2h3MzVsM2hvaGRkdDY3bGUvaW1hZ2UucG5n',
+                         'name': 'Doctor Strange',
+                         'description': 'Ticket to Doctor Strange premier'}]
+                     ]
+        return render_template('main_page.html', list_data=list_data)
 
 
 if __name__ == '__main__':
